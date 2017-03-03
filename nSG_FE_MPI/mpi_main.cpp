5753f2919aebc9aaec87745f775462df78fdeff1
mfkieler <mfkieler@stud.ntnu.no>
2017-03-03 08:34:19 +0100

    added nSG scheme

--- nSG_FE_MPI/mpi_main.cpp
+++ nSG_FE_MPI/mpi_main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    int N = 10000;
-    solver<PDE, double> s(N,world_size,world_rank,0.0,0.01);
+    int N = 1000;
+    solver<nSG, std::complex<double> > s(N,world_size,world_rank,0.0,0.001, -M_PI, M_PI);
     s.run();
 
 
--- nSG_FE_MPI/solver.hpp
+++ nSG_FE_MPI/solver.hpp
@@ -4,10 +4,11 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <math.h>
+#include <complex>
 
 
 
-struct PDE
+struct Heat_eq
 {
     static inline int topo_left_bound(int rank, int size){
         return (size + rank - 1) % size;
@@ -18,18 +19,50 @@ struct PDE
     static inline double set_initial_values(double x) {
         return std::sin(M_PI * x);
     }
-    static inline double eval_with_left_bound(double* u, double b, double r) {
+    static inline double eval_with_left_bound(double* u, double b, double r, double k) {
         return u[0] + r*(b - 2*u[0] + u[1]);
     }
-    static inline double eval_with_right_bound(double* u, double b, double r, int size) {
+    static inline double eval_with_right_bound(double* u, double b, double r, int size, double k) {
         return u[size-1] + r*(u[size-2] - 2*u[size-1] + b);
     }
-    static inline double eval(double* u, int i, double r) {
+    static inline double eval(double* u, int i, double r, double k) {
         return u[i] + r*(u[i-1] - 2*u[i] + u[i+1]);
     }
     static inline void write_data_to_file(double* u, int size){
         for (int i=0; i<size; i++) {
             std::cout << u[i] << " ";
+        }
+        std::cout << "\n";
+    }
+};
+
+struct nSG
+{
+    static inline int topo_left_bound(int rank, int size){
+        return (size + rank - 1) % size;
+    }
+    static inline int topo_right_bound(int rank, int size){
+        return (rank + 1) % size;
+    }
+    static inline std::complex<double> set_initial_values(double x) {
+        std::complex<double> I(0.0,1.0);
+        return std::exp(I * x);
+    }
+    static inline std::complex<double> eval_with_left_bound(std::complex<double>* u, std::complex<double> b, double r, double k) {
+        std::complex<double> I(0,1);
+        return u[0] + I*r*(b - 2.0*u[0] + u[1]) - I*0.5*k*std::abs(u[0])*std::abs(u[0])*u[0];
+    }
+    static inline std::complex<double> eval_with_right_bound(std::complex<double>* u, std::complex<double> b, double r, int size, double k) {
+        std::complex<double> I(0,1);
+        return u[size-1] + I*r*(u[size-2] - 2.0*u[size-1] + b) - I*0.5*k*std::abs(u[size-1])*std::abs(u[size-1])*u[size-1];
+    }
+    static inline std::complex<double> eval(std::complex<double>* u, int i, double r, double k) {
+        std::complex<double> I(0,1);
+        return u[i] + I*r*(u[i-1] - 2.0*u[i] + u[i+1]) - I*0.5*k*std::abs(u[i])*std::abs(u[i])*u[i];
+    }
+    static inline void write_data_to_file(std::complex<double>* u, int size){
+        for (int i=0; i<size; i++) {
+            std::cout << std::abs(u[i]) << " ";
         }
         std::cout << "\n";
     }
@@ -50,11 +83,12 @@ private:
     DATA_T *u;
     double ta, te, dt;
     double h, r;
+    double xa, xe;
     DATA_T lbound, rbound;
     int left_nb, right_nb;
 
 public:
-    explicit solver(int N, int size, int rank, double t1, double t2) {
+    explicit solver(int N, int size, int rank, double t1, double t2, double x1, double x2) {
         std::cout << "Init " << rank << "\n";
         world_rank = rank;
         world_size = size;
@@ -62,8 +96,10 @@ public:
         u = new DATA_T[reduced_sys_size];
         ta = t1;
         te = t2;
-        h = 1.0 / ((double) (N+1)) ;
-        dt = h*h/2/1.2;
+        xa = x1;
+        xe = x2;
+        h = (xe-xa) / ((double) (N+1)) ;
+        dt = h*h/2/1000;
         r = dt/h/h;
         std::cout << dt << "\n";
         left_nb = FUNC::topo_left_bound(world_rank, world_size);
@@ -81,7 +117,7 @@ void solver<FUNC, DATA_T>::run() {
 
         #pragma omp parallel for
         for (int k=0; k<reduced_sys_size; k++) {
-            u[k] = FUNC::set_initial_values( (world_rank*reduced_sys_size + k)*h );
+            u[k] = FUNC::set_initial_values( (world_rank*reduced_sys_size + k)*h + xa );
         }
         std::cout << "Data initiated in " << world_rank << "\n";
 
@@ -90,10 +126,10 @@ void solver<FUNC, DATA_T>::run() {
             MPI_Send(&u[reduced_sys_size-1], 1, MPI_DOUBLE, right_nb, 0, MPI_COMM_WORLD);
             MPI_Recv(&rbound, 1, MPI_DOUBLE, right_nb, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             MPI_Recv(&lbound, 1, MPI_DOUBLE, left_nb, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            u[0] = FUNC::eval_with_left_bound(u, lbound, r);
-            u[reduced_sys_size-1] = FUNC::eval_with_right_bound(u, rbound, r, reduced_sys_size);
+            u[0] = FUNC::eval_with_left_bound(u, lbound, r, dt);
+            u[reduced_sys_size-1] = FUNC::eval_with_right_bound(u, rbound, r, reduced_sys_size, dt);
             for (int i=1; i<reduced_sys_size-1; i++) {
-                u[i] = FUNC::eval(u, i, r);
+                u[i] = FUNC::eval(u, i, r, dt);
             }
             ta += dt;
         }
